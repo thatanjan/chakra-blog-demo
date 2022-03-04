19c6c4741b8124a1fd8d256e8ea51f2b9eb50fb8
thatanjan <anjan.cules@gmail.com>
2022-03-04 15:52:56 +0600

    toggle color mode feature is added

---
+++ src/components/Buttons/ToggleColorModeButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { IconButton, useColorMode } from '@chakra-ui/react'
+
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+
+const ToggleColorModeButton = () => {
+	const { colorMode, toggleColorMode } = useColorMode()
+
+	return (
+		<>
+			<IconButton
+				icon={colorMode === 'dark' ? <MoonIcon /> : <SunIcon />}
+				size='lg'
+				onClick={toggleColorMode}
+				sx={{
+					position: 'fixed',
+					bottom: '3rem',
+					right: '2rem',
+					zIndex: '1',
+				}}
+			/>
+		</>
+	)
+}
+
+export default ToggleColorModeButton
--- src/components/Layouts/PageLayout.jsx
+++ src/components/Layouts/PageLayout.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 
-import { Flex, Spacer, Box } from '@chakra-ui/react'
+import { Flex, Spacer, Box, useTheme } from '@chakra-ui/react'
 
 import AppHeader from '@/components/Headers/AppHeader'
+import ToggleColorModeButton from '@/components/Buttons/ToggleColorModeButton'
 
 const PageLayout = ({ children }) => {
+	console.log(useTheme())
 	return (
 		<>
 			<AppHeader />
 
 			{children}
+
+			<ToggleColorModeButton />
 		</>
 	)
 }
