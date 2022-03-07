0da82f36844c66687620575baba6b158d433da46
thatanjan <anjan.cules@gmail.com>
2022-03-07 10:53:31 +0600

    blog page datas can be fetched

---
+++ src/pages/blog/[slug].jsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react'
+
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
+
+import Blog from 'mongoose/Models/Blog'
+import connectDB from 'mongoose/connectDB'
+
+const BlogPage = ({ mdxSource }) => {
+	console.log(mdxSource)
+	return <h1>BlogPage</h1>
+}
+
+export const getStaticProps = async ({ params: { slug } }) => {
+	await connectDB()
+
+	const project = {
+		_id: 0,
+		__v: 0,
+	}
+
+	const result = await Blog.findOne({ slug }, project)
+
+	const { content, createdAt, ...blogData } = result.toObject()
+
+	const mdxSource = await serialize(content)
+
+	blogData.createdAt = createdAt.toDateString()
+
+	return {
+		props: {
+			mdxSource,
+			blogData,
+		},
+	}
+}
+
+export const getStaticPaths = async () => {
+	await connectDB()
+
+	const slugs = await Blog.find({}, 'slug')
+
+	const paths = slugs.map((item) => ({
+		params: { slug: item.slug },
+	}))
+
+	return {
+		paths,
+		fallback: false,
+	}
+}
+
+export default BlogPage
