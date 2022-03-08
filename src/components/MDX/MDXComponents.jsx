4a05c238eb36e004734da5ae8f140163d9198102
thatanjan <anjan.cules@gmail.com>
2022-03-08 20:51:10 +0600

    codeblock is fixed and new components are added

--- src/components/MDX/MDXComponents.jsx
+++ src/components/MDX/MDXComponents.jsx
@@ -1,18 +1,24 @@
-import { Heading, Text } from '@chakra-ui/react'
+import { Heading, Text, Code } from '@chakra-ui/react'
 import Image from 'next/image'
+import { YouTube } from 'mdx-embed'
 
-import {
-	ChakraTextLink,
-	ChakraImageLink,
-	Kbd,
-} from '@/components/Links/ChakraLink'
+import { ChakraTextLink } from '@/components/Links/ChakraLink'
 import CodeBlock from './CodeBlock'
 
-const components = { Image }
+const components = {
+	// custom
+	Image,
+	pre: CodeBlock,
+	a: ChakraTextLink,
 
-const headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+	// embed components
+	YouTube,
+
+	// chakra components
+}
 
 ;(() => {
+	const headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 	let length = headings.length
 
 	headings.forEach((tag) => {
@@ -26,10 +32,6 @@ const headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 
 components.p = (props) => <Text fontSize={['md', 'lg', 'xl']} {...props} />
 
-components.a = ChakraTextLink
-
-components.code = CodeBlock
-
-// components.code = (props) => <Kbd {...props} />
+components.code = (props) => <Code fontSize={['md', 'lg', 'xl']} {...props} />
 
 export default components
