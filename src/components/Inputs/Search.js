d9c93edec8b69f1b3be23e2ab0a3b17b7703cb5a
thatanjan <anjan.cules@gmail.com>
2022-03-11 14:08:36 +0600

    search component view is ready

--- src/components/Inputs/Search.js
+++ src/components/Inputs/Search.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { nanoid } from 'nanoid'
 
 import {
 	Box,
@@ -9,14 +10,84 @@ import {
 	chakra,
 	Flex,
 	Center,
+	Text,
+	HStack,
 } from '@chakra-ui/react'
 
 import { SearchIcon } from '@chakra-ui/icons'
 
+import ChakraNextLink from 'components/Links/ChakraLink'
+
+const EnterIcon = (props) => {
+	return (
+		<chakra.svg
+			strokeWidth='2px'
+			width='16px'
+			height='16px'
+			viewBox='0 0 20 20'
+			{...props}
+		>
+			<g
+				stroke='currentColor'
+				fill='none'
+				fillRule='evenodd'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			>
+				<path d='M18 3v4c0 2-2 4-4 4H2' />
+				<path d='M8 17l-6-6 6-6' />
+			</g>
+		</chakra.svg>
+	)
+}
+
+const BlogResult = ({ searchResults }) => {
+	return searchResults.map(() => (
+		<ChakraNextLink
+			chakraLinkProps={{
+				_hover: {
+					textDecoration: 'none',
+				},
+			}}
+			href='/'
+			key={nanoid()}
+			ChakraComponent={Box}
+		>
+			<Flex
+				_hover={{
+					background: 'teal.500',
+				}}
+				sx={{
+					minH: 16,
+					mt: 2,
+					px: 4,
+					py: 2,
+					alignItems: 'center',
+					rounded: 'lg',
+					fontWeight: 'semibold',
+					fontSize: 'lg',
+				}}
+			>
+				<Text flexGrow={1} noOfLines={2}>
+					This is a blog result blog result
+				</Text>
+
+				<EnterIcon ml='1rem' />
+			</Flex>
+
+			{/* <HStack spacing='1rem' wrap='wrap' textTransform='uppercase'> */}
+			{/* 	<Text>THU MARCH 21, 2020</Text> */}
+			{/* 	<Text>5 views</Text> */}
+			{/* 	<Text>5 min read</Text> */}
+			{/* </HStack> */}
+		</ChakraNextLink>
+	))
+}
+
 const Search = ({ onClose, isOpen }) => {
 	const [queryText, setQueryText] = useState('')
 
-	const [searchResults, setSearchResults] = useState([])
+	const [searchResults, setSearchResults] = useState(Array(10).fill(''))
 
 	const handleChange = (e) => {
 		setQueryText(e.target.value)
@@ -68,11 +139,7 @@ const Search = ({ onClose, isOpen }) => {
 							}}
 						>
 							<Box as='ul' borderTopWidth='1px' pt={2} pb={4}>
-								{Array(10)
-									.fill(0)
-									.map(() => (
-										<h1>Hello</h1>
-									))}
+								<BlogResult searchResults={searchResults} />
 							</Box>
 						</Box>
 					</ModalBody>
