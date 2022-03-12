724152c47d21a3e05f9a4f44ac83244688072b5b
thatanjan <anjan.cules@gmail.com>
2022-03-12 13:56:41 +0600

    overflow is now scroll and copy btn is added

--- src/components/MDX/CodeBlock.jsx
+++ src/components/MDX/CodeBlock.jsx
@@ -1,10 +1,14 @@
 /* eslint react/jsx-key: 0 */
 
-import React from 'react'
+import { useState } from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import vsDark from 'prism-react-renderer/themes/vsDark'
 
+import { Button, Flex } from '@chakra-ui/react'
+
 const CodeBlock = ({ children }) => {
+	const [codeCopied, setCodeCopied] = useState(false)
+
 	if (!children) return null
 
 	const {
@@ -12,6 +16,15 @@ const CodeBlock = ({ children }) => {
 	} = children
 
 	const language = className ? className.replace(/language-/, '') : ''
+
+	const handleCopy = () => {
+		navigator.clipboard.writeText(code)
+		setCodeCopied(true)
+
+		setTimeout(() => {
+			setCodeCopied(false)
+		}, 2000)
+	}
 
 	return (
 		<Highlight
@@ -21,18 +34,30 @@ const CodeBlock = ({ children }) => {
 			language={language}
 		>
 			{({ className, style, tokens, getLineProps, getTokenProps }) => (
-				<pre
-					className={className}
-					style={{ ...style, padding: '20px', overflowX: 'scroll' }}
-				>
-					{tokens.map((line, i) => (
-						<div key={i} {...getLineProps({ line, key: i })}>
-							{line.map((token, key) => (
-								<span key={key} {...getTokenProps({ token, key })} />
-							))}
-						</div>
-					))}
-				</pre>
+				<>
+					<pre
+						className={className}
+						style={{
+							...style,
+							padding: '30px',
+							overflowX: 'auto',
+						}}
+					>
+						<Flex justifyContent='end'>
+							<Button textTransform='uppercase' onClick={handleCopy}>
+								{codeCopied ? 'Copied' : 'Copy'}
+							</Button>
+						</Flex>
+
+						{tokens.map((line, i) => (
+							<div key={i} {...getLineProps({ line, key: i })}>
+								{line.map((token, key) => (
+									<span key={key} {...getTokenProps({ token, key })} />
+								))}
+							</div>
+						))}
+					</pre>
+				</>
 			)}
 		</Highlight>
 	)
