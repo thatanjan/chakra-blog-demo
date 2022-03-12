7947561827ef429f2c041b2fe93d7a0f026e161b
thatanjan <anjan.cules@gmail.com>
2022-03-12 19:49:49 +0600

    new features are added

--- src/components/Links/ChakraLink.jsx
+++ src/components/Links/ChakraLink.jsx
@@ -9,18 +9,31 @@ import {
 } from '@chakra-ui/react'
 
 const ChakraNextLink = ({
-	ChakraComponent,
+	ChakraComponent = Box,
 	href,
 	children,
-	chakraLinkProps,
+	chakraLinkProps = {},
 	overlay = false,
+	noOutline,
+	noUnderline,
+	color,
 	...props
 }) => {
 	const LinkComponent = overlay ? LinkOverlay : ChakraLink
 
+	if (noOutline) {
+		chakraLinkProps._focus = chakraLinkProps._focus || {}
+		chakraLinkProps._focus.outline = 0
+	}
+
+	if (noUnderline) {
+		chakraLinkProps._hover = chakraLinkProps._hover || {}
+		chakraLinkProps._hover.textDecoration = 'none'
+	}
+
 	return (
 		<Link href={href} passHref>
-			<LinkComponent {...chakraLinkProps}>
+			<LinkComponent color={color} {...chakraLinkProps}>
 				<ChakraComponent {...props}>{children}</ChakraComponent>
 			</LinkComponent>
 		</Link>
@@ -28,11 +41,11 @@ const ChakraNextLink = ({
 }
 
 const ChakraTextLink = (props) => (
-	<ChakraNextLink ChakraComponent={Text} {...props} />
+	<ChakraNextLink ChakraComponent={Text} {...props} color='teal.300' />
 )
 
 const ChakraButtonLink = (props) => (
-	<ChakraNextLink ChakraComponent={Button} {...props} />
+	<ChakraNextLink ChakraComponent={Button} {...props} noUnderline />
 )
 
 const ChakraImageLink = (props) => (
@@ -43,6 +56,6 @@ const ChakraHeadingLink = (props) => (
 	<ChakraNextLink ChakraComponent={Heading} {...props} />
 )
 
-export { ChakraTextLink, ChakraButtonLink, ChakraImageLink }
+export { ChakraTextLink, ChakraButtonLink, ChakraImageLink, ChakraHeadingLink }
 
 export default ChakraNextLink
