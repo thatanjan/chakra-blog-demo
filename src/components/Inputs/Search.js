9d5de4075671f27558edd77239d2b4aaf5824d0c
thatanjan <anjan.cules@gmail.com>
2022-03-12 12:31:31 +0600

    Input component is done

--- src/components/Inputs/Search.js
+++ src/components/Inputs/Search.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { nanoid } from 'nanoid'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import {
 	Box,
@@ -10,88 +10,46 @@ import {
 	chakra,
 	Flex,
 	Center,
-	Text,
-	HStack,
+	useColorMode,
 } from '@chakra-ui/react'
 
 import { SearchIcon } from '@chakra-ui/icons'
 
-import ChakraNextLink from 'components/Links/ChakraLink'
-
-const EnterIcon = (props) => {
-	return (
-		<chakra.svg
-			strokeWidth='2px'
-			width='16px'
-			height='16px'
-			viewBox='0 0 20 20'
-			{...props}
-		>
-			<g
-				stroke='currentColor'
-				fill='none'
-				fillRule='evenodd'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-			>
-				<path d='M18 3v4c0 2-2 4-4 4H2' />
-				<path d='M8 17l-6-6 6-6' />
-			</g>
-		</chakra.svg>
-	)
-}
-
-const BlogResult = ({ searchResults }) => {
-	return searchResults.map(() => (
-		<ChakraNextLink
-			chakraLinkProps={{
-				_hover: {
-					textDecoration: 'none',
-				},
-			}}
-			href='/'
-			key={nanoid()}
-			ChakraComponent={Box}
-		>
-			<Flex
-				_hover={{
-					background: 'teal.500',
-				}}
-				sx={{
-					minH: 16,
-					mt: 2,
-					px: 4,
-					py: 2,
-					alignItems: 'center',
-					rounded: 'lg',
-					fontWeight: 'semibold',
-					fontSize: 'lg',
-				}}
-			>
-				<Text flexGrow={1} noOfLines={2}>
-					This is a blog result blog result
-				</Text>
-
-				<EnterIcon ml='1rem' />
-			</Flex>
-
-			{/* <HStack spacing='1rem' wrap='wrap' textTransform='uppercase'> */}
-			{/* 	<Text>THU MARCH 21, 2020</Text> */}
-			{/* 	<Text>5 views</Text> */}
-			{/* 	<Text>5 min read</Text> */}
-			{/* </HStack> */}
-		</ChakraNextLink>
-	))
-}
+import BlogSearchResult from 'components/Blog/BlogSearchResult'
 
 const Search = ({ onClose, isOpen }) => {
 	const [queryText, setQueryText] = useState('')
 
-	const [searchResults, setSearchResults] = useState(Array(10).fill(''))
+	const [searchResults, setSearchResults] = useState([])
 
-	const handleChange = (e) => {
-		setQueryText(e.target.value)
+	const { colorMode } = useColorMode()
+
+	const backgroundColor = {
+		light: 'white',
+		dark: 'gray.700',
 	}
+
+	const handleChange = (e) => setQueryText(e.target.value)
+
+	useEffect(() => {
+		if (!queryText) return false
+
+		const cancelTokenSource = axios.CancelToken.source()
+
+		;(async () => {
+			try {
+				const { data } = await axios.get('/api/search', {
+					params: {
+						query: queryText,
+					},
+				})
+
+				setSearchResults(data)
+			} catch (_) {}
+		})()
+
+		return () => cancelTokenSource.cancel()
+	}, [queryText])
 
 	return (
 		<div>
@@ -118,8 +76,7 @@ const Search = ({ onClose, isOpen }) => {
 								pl: '68px',
 								fontWeight: 'medium',
 								outline: 0,
-								bg: 'white',
-								'.chakra-ui-dark &': { bg: 'gray.700' },
+								bg: backgroundColor[colorMode],
 							}}
 							placeholder='Search blogs'
 							value={queryText}
@@ -130,19 +87,20 @@ const Search = ({ onClose, isOpen }) => {
 						</Center>
 					</Flex>
 
-					<ModalBody maxH='70vh' p='0'>
-						<Box
-							sx={{
-								px: 4,
-								bg: 'white',
-								'.chakra-ui-dark &': { bg: 'gray.700' },
-							}}
-						>
-							<Box as='ul' borderTopWidth='1px' pt={2} pb={4}>
-								<BlogResult searchResults={searchResults} />
+					{searchResults.length > 0 && (
+						<ModalBody maxH='70vh' p='0'>
+							<Box
+								sx={{
+									px: 4,
+									bg: backgroundColor[colorMode],
+								}}
+							>
+								<Box as='ul' borderTopWidth='1px' pt={2} pb={4}>
+									<BlogSearchResult onClose={onClose} searchResults={searchResults} />
+								</Box>
 							</Box>
-						</Box>
-					</ModalBody>
+						</ModalBody>
+					)}
 				</ModalContent>
 			</Modal>
 		</div>
